ff4b13b2954df0cb5830dde934edcb4b884ad3bc
IvanCacereZ <130480223+IvanCacereZ@users.noreply.github.com>
2024-05-28 01:34:57 -0500

    PrototypeVersion

---
+++ Assets/Scripts/Prototype/VisionSensorPrimitive.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionSensorPrimitive : MonoBehaviour
+{
+    [Header("Opciones")]
+    public SphereCollider visionCollider;
+    public LayerMask detectableLayers;
+    public float visionRange = 4.0f;
+    public float speed = 2.0f;
+    public float stopDistance = 2.0f;
+
+    [Header("Personaje Detectado")]
+    public GameObject objectCollision;
+
+    private bool isObjectDetected = false;
+
+    private void Start()
+    {
+        visionCollider.radius = visionRange;
+    }
+
+    private void Update()
+    {
+        if (!isObjectDetected)
+        {
+            objectCollision = null;
+        }
+        else if (objectCollision != null)
+        {
+            MoveTowardsObject();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isObjectDetected && ((1 << other.gameObject.layer) & detectableLayers) != 0)
+        {
+            objectCollision = other.gameObject;
+            isObjectDetected = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (objectCollision == other.gameObject)
+        {
+            isObjectDetected = false;
+            objectCollision = null;
+        }
+    }
+
+    void MoveTowardsObject()
+    {
+        Vector3 direction = (objectCollision.transform.position - transform.position).normalized;
+        float distance = Vector3.Distance(transform.position, objectCollision.transform.position);
+
+        if (distance > stopDistance)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}
