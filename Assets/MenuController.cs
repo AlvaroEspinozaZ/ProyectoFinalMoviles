eff741f46cdde8bd60234a5777748ec6f264c1fe
unknown <alvaro.espinoza.z@tecsup.edu.pe>
2024-06-04 15:43:53 -0500

    DownLoadDoTween

---
+++ Assets/MenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class MenuController : MonoBehaviour
+{
+    public GameObject texto;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
